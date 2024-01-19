6ad4e2f9311a52d5784638b41e5a481627517a2b
kate <46572320+katelmao@users.noreply.github.com>
2024-01-19 20:07:22 +0000

    feat: change hotkey

--- web/js/loop.js
+++ web/js/loop.js
@@ -312,7 +312,7 @@ setInterval(retrieve_stock,stocking_timeout);
 
 // input
 document.addEventListener('keydown', function(event) {
-    if (event.ctrlKey && event.key === 'a') {
+    if (event.ctrlKey && event.key === 'q') {
         add_artist_modal();
         event.preventDefault();
     }
